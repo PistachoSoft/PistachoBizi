9b83da3c93fda4b897b2bcee56f8171b59dfd78b
RecuencoJones <david.recuencogadea@gmail.com>
2015-06-02 22:28:37 +0200

    API specs via swagger.yaml

--- src/main/webapp/js/services/statsService.js
+++ src/main/webapp/js/services/statsService.js
@@ -57,5 +57,5 @@ angular.module('pistachoBizi')
             M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
             if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
             return M.join(' ');
-        }
+        };
     }]);
